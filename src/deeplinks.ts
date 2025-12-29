e579d9c9580fa30f21059a65b2912dac303d139d
Aylon, Noa <noa.aylon@dynatrace.com>
2025-12-29 14:00:09 +0200

    APPOBS-31134 handle both rookout:// and dynatrace:// URL schemas

--- src/deeplinks.ts
+++ src/deeplinks.ts
@@ -2,22 +2,21 @@ import {dialog} from "electron";
 
 const path = require("path");
 
-const PROTOCOL = "rookout";
-
-
+const PROTOCOLS = ["dynatrace", "rookout"];
 
 export const initDeeplinks = (app: Electron.App) => {
-    if (process.defaultApp) {
-        if (process.argv.length >= 2) {
-            app.setAsDefaultProtocolClient(PROTOCOL, process.execPath, [path.resolve(process.argv[1])]);
+    PROTOCOLS.forEach(protocol => {
+        if (process.defaultApp) {
+            if (process.argv.length >= 2) {
+                app.setAsDefaultProtocolClient(protocol, process.execPath, [path.resolve(process.argv[1])]);
+            }
+        } else {
+            app.setAsDefaultProtocolClient(protocol);
         }
-    } else {
-        app.setAsDefaultProtocolClient(PROTOCOL);
-    }
+    });
 
     app.on("open-url", deeplinkHandler);
 };
-
 
 export const deeplinkHandler = () => {
    dialog.showMessageBoxSync({title: "Dynatrace Desktop App", message: "Dynatrace Desktop App is now running"});
